7f0e63ba4882e80f6189d7041dcbef568d0a0627
cyclo9 <32120227+cyclo9@users.noreply.github.com>
2023-05-26 14:30:30 -0700

    Assert that the MONGODB_URI will never be undefined

--- app/lib/mongo.ts
+++ app/lib/mongo.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI
-const client = new MongoClient(uri);
+const client = new MongoClient(uri!);
 const db = client.db("database");
 
 export default db;
